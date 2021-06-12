628fd7e2f461134497f5ed8a25b68877e6512c3a
spaulsteinberg <sampaulsteinberg@gmail.com>
2021-06-12 12:09:24 -0500

    GET call for dbs, need to set up redux

--- src/components/main/content/Dashboard.js
+++ src/components/main/content/Dashboard.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {db} from'../../../firebase';
 
 const Dashboard = () => {
+
+    const [dbData, setDbData] = useState();
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                await db.collection(process.env.REACT_APP_ORDER_DB_COLLECTION)
+                    .get()
+                    .then(response => response.docs)
+                    .then(docs => docs.map(d => console.log(d.data())))
+                    .catch(err => console.log(err))
+            } catch (err) {
+                console.log("here")
+                setError(err)
+            } finally {
+                console.log("here finally")
+                setLoading(false)
+            }
+        }
+        fetchData()
+    }, [])
 
     return (
         <div>
