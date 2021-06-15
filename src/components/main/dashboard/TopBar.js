9f6d30f030bba2206a5397e4b2d89bf1e43b31f5
spaulsteinberg <sampaulsteinberg@gmail.com>
2021-06-15 09:48:09 -0500

    close #29 responsive button w api call done

--- src/components/main/dashboard/TopBar.js
+++ src/components/main/dashboard/TopBar.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import TopBarIconShortcut from './TopBarIconShortcut';
-import { cashCoinIconPaths, cartIconPath, tagIconPath } from '../../../constants/svg/svgs';
-import {Link} from 'react-router-dom'
-
+import { cashCoinIconPaths, cartIconPath, tagIconPath, refreshIconPath } from '../../../constants/svg/svgs';
+import {Link} from 'react-router-dom';
+import { getAllOrders } from '../../../redux/orders/orderActions';
+import { useDispatch } from 'react-redux';
 
 const TopBar = (props) => {
+    const dispatch = useDispatch();
     let iconData = [
         {
             element: (<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="green" viewBox="0 0 16 16">
@@ -37,11 +39,27 @@ const TopBar = (props) => {
             data: `$${props.avgOrderPrice().toFixed(2)}`,
             timeframe: "Last Week"
         }
-    ]
+    ];
+
+    const handleRefreshClick = event => {
+        event.preventDefault();
+        dispatch(getAllOrders())
+    }
     return (
         <Card className="mt-3">
-            <Card.Header>
-                <h2 className="profile-header mb-2">Recent Performance</h2>
+            <Card.Header className="dashboard-top-container">
+                <div className="empty-div"></div>
+                <div className="dashboard-top-header-container">
+                    <h2 className="profile-header mb-2">Recent Performance</h2>
+                </div>
+                <div className="dashboard-top-refresh-container">
+                    <button type="button" className="btn btn-secondary" id="refresh-dashboard-button" onClick={handleRefreshClick}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                            <path fillRule="evenodd" d={refreshIconPath[0]} />
+                            <path d={refreshIconPath[1]} />
+                        </svg>
+                    </button>
+                </div>
             </Card.Header>
             <Card.Body className="top-nav-body-wrapper">
                 {iconData.map((icon, ind) => <TopBarIconShortcut iconData={icon} key={ind} />)}
