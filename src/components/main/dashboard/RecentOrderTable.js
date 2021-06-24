0381e2ccf3bd5ce1f84af073c21d190ec0b6c8fd
spaulsteinberg <sampaulsteinberg@gmail.com>
2021-06-24 14:43:42 -0500

    #38 order table pre flex

--- src/components/main/dashboard/RecentOrderTable.js
+++ src/components/main/dashboard/RecentOrderTable.js
@@ -1,38 +1,47 @@
 import React from 'react'
+import { Card } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 
-const RecentOrderTable = () => {
-  /* # will be maske receipt num, include date, amount spent */
+const RecentOrderTable = (props) => {
+    const recentOrders = useSelector(state => state.orders.data.map(order => {
+                                  return {
+                                    id: order.receiptNumber.substring(0, 5) + 'x'.repeat(5),
+                                    date: order.date,
+                                    name: `${order.firstName[0]}. ${order.lastName}`,
+                                    revenue: `$${order.totalCost}`
+                                  }
+                                })
+                                .sort((a, b) => new Date(b.date) - new Date(a.date))
+                                .slice(0, 10));
+    const colNames = ["Order #", "Date", "Name","Amount"]
+  
     return (
-        <table className="table table-bordered table-striped">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry the Bird</td>
-      <td>@twitter</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+      <Card>
+        <Card.Header className="dashboard-table-card-header"><h2>Recent Orders</h2></Card.Header>
+        <Card.Body>
+          <div className="table-responsive">
+            <table className="table table-striped" id="dashboard-order-table">
+              <thead>
+                <tr>
+                  {colNames.map((name, i) => <th scope="col" key={i} className="table-info">{name}</th>)}
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  recentOrders.map(order =>
+                    <tr key={order.id}>
+                      <th scope="row">{order.id}</th>
+                      <td>{order.date}</td>
+                      <td>{order.name}</td>
+                      <td>{order.revenue}</td>
+                    </tr>
+                  )
+                }
+              </tbody>
+            </table>
+          </div>
+        </Card.Body>
+      </Card>
     )
 }
 
